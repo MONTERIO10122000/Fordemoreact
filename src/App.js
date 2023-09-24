a701539f90f0f437476e126c37a5db75135de8ff
MONTERIO10122000 <bisal.das2000@gmail.com>
2023-09-24 22:03:42 +0530

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import User from './component1';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello world</h1>
+      <User/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component1.js
@@ -0,0 +1,9 @@
+function User(){
+    return(
+        <div>
+            <h1>This is a component</h1>
+            <p>Here it is..</p>
+        </div>
+    )
+}
+export default User;
